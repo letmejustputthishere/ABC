85337d27c73e1f8fbad361bcda73364747fa3b87
Alexander Viand <alexander.viand@inf.ethz.ch>
2020-02-10 11:40:52 +0100

    Add Doc to CKKSRewriter referencing CKKS and EVA

--- include/optimizer/CKKSRewriter.h
+++ include/optimizer/CKKSRewriter.h
@@ -3,11 +3,26 @@
 
 #include "Ast.h"
 
+/// Introduces the ciphertext maintenance operations (rescale, relinearize, modulus switching, etc)
+/// required in the CKKS Scheme (CKKS 2017) into an AST, i.e. decides when during the computation to apply them.
+/// This is done in a near-optimal way, based on ideas from EVA (DRKS+ 2019)
+///
+/// Cheon, Jung Hee, Andrey Kim, Miran Kim, and Yongsoo Song. 2017.
+/// “Homomorphic Encryption for Arithmetic of Approximate Numbers.”
+/// In Advances in Cryptology – ASIACRYPT 2017, 409–37. Springer International Publishing.
+///
+/// Dathathri, Roshan, Blagovesta Kostova, Olli Saarikivi, Wei Dai, Kim Laine, and Madanlal Musuvathi. 2019.
+/// “EVA: An Encrypted Vector Arithmetic Language and Compiler for Efficient Homomorphic Computation.”
+/// arXiv [cs.CR]. arXiv. http://arxiv.org/abs/1912.11951.
+///
 class CKKSRewriter {
 public:
-    explicit CKKSRewriter(Ast& ast);
+    explicit CKKSRewriter(Ast &ast);
 
-    Ast& applyCKKSRewriting();
+    /// Add ciphertext maintenance operations required for CKKS to the AST
+    /// This modifies the AST that was passed when the object was created
+    /// \return Reference to the rewritten AST
+    Ast &applyCKKSRewriting();
 
 };
 
